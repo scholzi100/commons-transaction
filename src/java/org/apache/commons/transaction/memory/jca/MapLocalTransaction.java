9bf426005568aa38a1f8c17aff7eb1218aaa40f7
Oliver Zeigermann <ozeigermann@apache.org>
2004-11-19 23:41:29 +0000

    Added missing license

--- src/java/org/apache/commons/transaction/memory/jca/MapLocalTransaction.java
+++ src/java/org/apache/commons/transaction/memory/jca/MapLocalTransaction.java
@@ -1,9 +1,26 @@
 /*
- * Created on 17.05.2004
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//transaction/src/java/org/apache/commons/transaction/memory/jca/MapLocalTransaction.java,v 1.2 2004/11/19 23:41:29 ozeigermann Exp $
+ * $Revision: 1.2 $
+ * $Date: 2004/11/19 23:41:29 $
  *
- * To change this generated comment go to 
- * Window>Preferences>Java>Code Generation>Code Template
+ * ====================================================================
+ *
+ * Copyright 1999-2002 The Apache Software Foundation 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
+
 package org.apache.commons.transaction.memory.jca;
 
 import javax.resource.ResourceException;
@@ -12,7 +29,7 @@ import org.apache.commons.transaction.memory.TransactionalMapWrapper;
 
 /**
  * 
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  * 
  */
 public class MapLocalTransaction implements javax.resource.spi.LocalTransaction, javax.resource.cci.LocalTransaction {
